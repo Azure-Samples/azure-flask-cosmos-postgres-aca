fa8b7ae37a028baafec2e5227181eba7879679b5
Pamela Fox <pamela.fox@gmail.com>
2023-09-17 07:39:05 -0700

    Update to latest

---
+++ src/flaskapp/models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from . import db
+
+association_table = db.Table(
+    "cruise_destination_link",
+    Column("cruise_id", ForeignKey("cruise.id"), primary_key=True),
+    Column("destination_id", ForeignKey("destination.id"), primary_key=True),
+)
+
+
+class Destination(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255))
+    subtitle: Mapped[str | None] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(1000))
+    cruises: Mapped[list["Cruise"]] = relationship("Cruise", secondary=association_table, back_populates="destinations")
+
+    def __str__(self):
+        return self.name
+
+
+class Cruise(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255))
+    subtitle: Mapped[str | None] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(1000))
+    destinations: Mapped[list[Destination]] = relationship(
+        "Destination", secondary=association_table, back_populates="cruises"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class InfoRequest(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255))
+    email: Mapped[str] = mapped_column(String(255))
+    notes: Mapped[str] = mapped_column(String(255))
+    cruise_id: Mapped[int] = mapped_column(ForeignKey("cruise.id"))
